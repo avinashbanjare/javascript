86e2a4fa690b396368e8a10717c94a5d11d98431
avinash <akbanjare96@gmail.com>
2024-09-09 17:01:34 +0530

    numbers.js

---
+++ numbers.js
@@ -0,0 +1,50 @@
+/* mATHEMATICAL OPERATION (+, -, *, /, %) */
+
+// add
+const A = 30
+const B = 20 
+console.log (A+B)
+
+// subSTRACT
+const C = 50
+const D = 30
+console.log (C-D)
+
+//MULTIPLY
+const E =450
+const F =2
+console.log (E*F)
+
+// DIVIDE
+const G =300
+const H = 3
+console.log (G/H)
+
+// remider
+const I = 301
+const J = 3
+console.log (I%J)
+
+
+console.log("---------------------------------------");
+
+
+/* Comparison operation (< ,  > , ===) */   // =   --> value assign,        ===   -->  
+const z = 70
+const x = 50
+
+const m = 50
+const n = 50
+
+console.log(z < x);
+console.log(C>D);
+console.log(C<D);
+
+console.log(m===n);
+
+console.log();
+
+
+
+
+
